4b571926c921dc1335039924b511a8a4d9886956
unknown <gutleberb@gmail.com>
2019-02-19 11:17:47 -0600

    finished basic styling and reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,3 +10,25 @@ var imgVal3 = 0;
 var imgVal4 = 0;
 
 
+
+    function reset() {
+        imgVal1 = Math.floor(Math.random() * 12) + 1;
+        console.log("val 1: " + imgVal1);
+        imgVal2 = Math.floor(Math.random() * 12) + 1;
+        console.log("val 2: " + imgVal2);
+        imgVal3 = Math.floor(Math.random() * 12) + 1;
+        console.log("val 3: " + imgVal3);
+        imgVal4 = Math.floor(Math.random() * 12) + 1;
+        console.log("val 4: " + imgVal4);
+
+        scoreToWin = Math.floor(Math.random() * 102) + 19;
+        console.log("win: " + scoreToWin);
+
+        currentScore = 0;
+        totalWins = 0;
+        totalLoses = 0;
+}
+
+reset();
+
+
