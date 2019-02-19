fd9771fe0cbb7376f7d59d21041e5e17fbfbeb60
unknown <gutleberb@gmail.com>
2019-02-19 10:53:26 -0600

    setup the basic layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// create variables to store the required information
+
+var scoreToWin = 0;
+var currentScore = 0;
+var totalWins = 0;
+var totalLoses = 0;
+var imgVal1 = 0;
+var imgVal2 = 0;
+var imgVal3 = 0;
+var imgVal4 = 0;
+
+
